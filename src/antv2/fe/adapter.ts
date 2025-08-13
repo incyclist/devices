8a1874d8522a0ce47cd089f1c775fa084cf19199
Guido Doumen <incyclist@doumen-laumen.de>
2025-08-13 11:04:42 +0200

    remove debug log

--- src/antv2/fe/adapter.ts
+++ src/antv2/fe/adapter.ts
@@ -67,17 +67,7 @@ export default class AntFEAdapter extends AntAdapter<FitnessEquipmentSensorState
 
         // busy with previous update 
         if (this.promiseSendUpdate) {
-
-            let stack
-            try {
-                throw new Error()
-            }
-            catch(err) {
-                stack = err.stack
-            }
-
-
-            this.logEvent({message: 'send bike update skipped', device:this.getName(),request, reason:'busy',stack})
+            this.logEvent({message: 'send bike update skipped', device:this.getName(),request, reason:'busy'})
             return;
         }
 
